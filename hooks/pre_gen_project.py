8c32f7f89d133f32e456e7bec4c0e467eaac912c
michplunkett <michplunkett@gmail.com>
2023-03-13 12:57:00 -0500

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,7 +7,8 @@ MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{ cookiecutter.project_slug}}'
 
 if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: The {{ cookiecutter.project_slug }} slug (%s) is not a valid Python module name. '
-          'Please do not use a - and use _ instead' % module_name)
+    print('ERROR: The {{ cookiecutter.project_slug }} slug (%s) is not a '
+          'valid Python module name.  Please do not use a - and use _ instead'
+          % module_name)
 
     sys.exit(1)
