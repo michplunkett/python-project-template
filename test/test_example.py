e7e4ec846b4776f744d3bc9d158b264205e7e726
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-07-05 19:56:41 -0500

    Update test_example.py

--- test/test_example.py
+++ test/test_example.py
@@ -1,7 +1,6 @@
-"""
-This file serves as an example of a basic test.
-"""
+"""This file serves as an example of a basic test."""
 
 
 def test_example():
+    """Serves the basis for how tests are written within this package."""
     assert 2 + 2 == 4
